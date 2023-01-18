991137e71a98faa41b13fc8572c58d7cd4514abb
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2023-01-17 21:37:41 -0300

    adjusts vue.config for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
+module.exports = {
+  publicPath: "",
+};
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/",
 
   pluginOptions: {
     vuetify: {
