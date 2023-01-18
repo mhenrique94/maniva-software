d65146759efe680b95b11eb6fad168758fd14859
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2023-01-18 10:34:46 -0300

    adds vercel.json for auto-deploy x3

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/maniva-software/" : "/",
-
+  publicPath: "/",
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
