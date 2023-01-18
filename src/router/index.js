1b59dd7e7076c8a6e17f0e26ae202def1bf176f5
Marcelo Silva <45953439+mhenrique94@users.noreply.github.com>
2023-01-18 09:41:42 -0300

    trying again to deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const routes = [
@@ -16,14 +16,10 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
-  {
-    path: "*",
-    name: "catchAll",
-    component: HomeView,
-  },
 ];
 
 const router = createRouter({
+  history: createWebHashHistory(),
   routes,
 });
 
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/maniva-software/" : "",
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/maniva-software/" : "/",
+
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     },
   },
-};
+});
