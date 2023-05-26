82ec78f8c5292136579ac04b2f4e08e705eb2751
Marcelo Silva <mhenrique94@outlook.com>
2023-05-25 22:38:27 -0300

    start over

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar flat color="#00af66">
+    <v-app-bar-title class="text-white">
+      Maniva
+    </v-app-bar-title>
+    <v-spacer />
+  </v-app-bar>
+</template>
+
+<script>
+  //
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <router-view />
+<div></div>
 </template>
 
 <script>
-  //
+
 </script>
