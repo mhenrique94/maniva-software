0af18e144c4d3c089d48bfa03ce40628d37f4df4
mhenrique94 <mhenrique94@outlook.com>
2023-11-12 19:38:48 -0300

    navega pelo conteudo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,56 @@
 <template>
-  <NotionRenderer :blockMap="blockMap" fullPage />
+  <div v-if="blockMap" class="blog-wrapper">
+    <div v-if="pageToRender" class="bw-rendered-page">
+      <NotionRenderer :blockMap="pageToRender.blockmap" fullPage />
+    </div>
+    <aside>
+      <h4>Tabela de conteudo</h4>
+      <!-- <NotionRenderer :blockMap="blockMap" fullPage /> -->
+      <div v-for="(page, index) in posts" :key="index">
+        <a @click="setRender(page)">{{ page.title }}</a>
+      </div>
+    </aside>
+  </div>
 </template>
 
 <script>
 import { NotionRenderer, getPageBlocks } from "vue-notion"
 export default {
   components: { NotionRenderer },
-  data: () => ({ blockMap: null }),
+  data: () => ({ blockMap: null, posts: [], pageToRender: null}),
   async created() {
-    this.blockMap = await getPageBlocks("d47aa7000088417a8bb13d5c40d2f9d7")
+    const pageId = "d47aa7000088417a8bb13d5c40d2f9d7"
+    this.blockMap = await getPageBlocks(pageId)
+    for (const [key, value] of Object.entries(this.blockMap)) {
+      const childId = value.value.id.replaceAll("-", "")
+      if (value.value.type == "page" && pageId != childId) {
+        this.posts.push(
+          {
+            content: value.value.content,
+            title: value.value.properties.title[0][0],
+            blockmap: await getPageBlocks(value.value.id)
+          }
+        );
+      }
+      this.pageToRender = this.posts[0]
+    }
+  },
+  methods: {
+    setRender (page) {
+      this.pageToRender = page
+    }
   }
 }
 </script>
 <style>
 @import "vue-notion/src/styles.css";
+
+.blog-wrapper {
+  display: flex;
+  gap: 24px;
+}
+
+.bw-rendered-page {
+  padding: 24px;
+}
 </style>
